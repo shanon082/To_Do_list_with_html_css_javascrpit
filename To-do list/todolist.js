4e82b0156bb3b64e25ec437eba058ab049a33858
shanon_cymon <136820066+shanon082@users.noreply.github.com>
2024-05-02 16:53:42 +0300

    Add files via upload

---
+++ To-do list/todolist.js
@@ -0,0 +1,34 @@
+var inputBox = document.getElementById("inputbox");
+var listcontainer = document.getElementById("list_container");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("Fill in the List");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML= inputBox.value;
+        listcontainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML='\u00d7';
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+listcontainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+function saveData(){
+    localStorage.setItem("data", listcontainer.innerHTML);
+}
+function showTask(){
+    listcontainer.innerHTML = localStorage.getItem("data");
+}
